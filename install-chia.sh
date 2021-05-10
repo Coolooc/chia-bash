3b9c3356503747776c4c97e89ee4d7963a7aac94
Georgy Nalyotov <egor-nal@yandex.ru>
2021-05-10 18:14:23 +0400

    add install-chia.sh

---
+++ install-chia.sh
@@ -0,0 +1,10 @@
+#install-chia.sh
+#!/bin/bash
+cd ~
+sudo apt update
+sudo apt upgrade -y
+sudo apt install git -y
+git clone https://github.com/Chia-Network/chia-blockchain.git -b latest --recurse-submodules
+cd chia-blockchain
+sh install.sh
+#. ./activate
