4874dd32f745612d829bbc583521c7c6602e1dfd
Coolooc <egor-nal@yandex.ru>
2021-05-10 22:24:51 +0400

    Update install-chia.sh

--- install-chia.sh
+++ install-chia.sh
@@ -3,7 +3,7 @@
 cd ~
 sudo apt update
 sudo apt upgrade -y
-sudo apt install git -y
+sudo apt install git smartmontools -y
 git clone https://github.com/Chia-Network/chia-blockchain.git -b latest --recurse-submodules
 cd chia-blockchain
 sh install.sh
