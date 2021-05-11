52c9dc344d210de41235bd097930be2d7182438d
Coolooc <egor-nal@yandex.ru>
2021-05-11 23:27:07 +0400

    Update start-chia.sh

--- start-chia.sh
+++ start-chia.sh
@@ -5,6 +5,10 @@ cd ~/chia-blockchain
 chia start farmer
 echo "wait..."
 sleep 60
+#chia show -a introducer-apne.chia.net:8444
+#chia show -a introducer-apse.chia.net:8444
+#chia show -a introducer-or.chia.net:8444
+#chia show -a introducer-va.chia.net:8444
 chia show -a introducer-eu.chia.net:8444
 chia show -a node-eu.chia.net:8444
 echo Started.
