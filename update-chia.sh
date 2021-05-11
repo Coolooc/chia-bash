6d3c21093bfebe7953334f2520cbf6857e68ace1
Coolooc <egor-nal@yandex.ru>
2021-05-11 22:47:57 +0400

    Update update-chia.sh

--- update-chia.sh
+++ update-chia.sh
@@ -9,6 +9,7 @@ git status
 #If you get RELEASE.dev0 then delete the package-lock.json in chia-blockchain-gui and install.sh again
 #git status should say "nothing to commit, working tree clean",
 #if you have uncommitted changes, RELEASE.dev0 will be reported.
+echo ""
 echo ">>>If you get RELEASE.dev0 then delete the package-lock.json in chia-blockchain-gui and install.sh again"
 echo '>>>git status should say "nothing to commit, working tree clean",'
 echo ">>>if you have uncommitted changes, RELEASE.dev0 will be reported."
