072eef2921b372fbcf585092aa9b62e68a03aead
Coolooc <egor-nal@yandex.ru>
2021-05-12 13:59:34 +0400

    Update start-chia.sh

--- start-chia.sh
+++ start-chia.sh
@@ -3,8 +3,8 @@
 cd ~/chia-blockchain
 . ./activate
 chia start farmer
-echo "wait..."
-sleep 60
+echo "wait 2 min..."
+sleep 120
 #chia show -a introducer-apne.chia.net:8444
 #chia show -a introducer-apse.chia.net:8444
 #chia show -a introducer-or.chia.net:8444
@@ -12,4 +12,6 @@ sleep 60
 chia show -a introducer-eu.chia.net:8444
 chia show -a node-eu.chia.net:8444
 #https://chia.keva.app/
+cd ~/chia-bash
+./new-nodes-chia.sh
 echo Started.
