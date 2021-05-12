bb02616ea51cbcdb31e373216746b63e131f2020
Coolooc <egor-nal@yandex.ru>
2021-05-12 13:30:56 +0400

    Create new-node-chia.sh

---
+++ new-node-chia.sh
@@ -0,0 +1,14 @@
+#new-node-chia.sh
+#!/bin/bash
+cd ~/chia-blockchain
+. ./activate
+curl -G https://chia.keva.app/ | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' > nodes.txt
+i=1
+while read LINE; do
+	echo $i
+	chia show -a $LINE:8444
+	let i++
+done < nodes.txt
+rm nodes.txt
+echo "New nodes added."
+chia show -c
