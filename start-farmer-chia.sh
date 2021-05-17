8a02a7a67622e5bd7f2fca14d7a223373e489783
Coolooc <egor-nal@yandex.ru>
2021-05-17 10:44:00 +0400

    Create start-farmer-chia.sh

---
+++ start-farmer-chia.sh
@@ -0,0 +1,17 @@
+#start-farmer-chia.sh
+#!/bin/bash
+cd ~/chia-blockchain
+. ./activate
+chia start farmer
+echo "wait 2 min..."
+sleep 120
+#chia show -a introducer-apne.chia.net:8444
+#chia show -a introducer-apse.chia.net:8444
+#chia show -a introducer-or.chia.net:8444
+#chia show -a introducer-va.chia.net:8444
+chia show -a introducer-eu.chia.net:8444
+chia show -a node-eu.chia.net:8444
+#https://chia.keva.app/
+echo Started.
+cd ~/chia-bash
+./new-nodes-chia.sh
