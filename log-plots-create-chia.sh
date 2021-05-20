f5706a26f6d5f44da55426711ac4cbdca225e769
Coolooc <egor-nal@yandex.ru>
2021-05-20 21:37:32 +0400

    Update log-plots-create-chia.sh

--- log-plots-create-chia.sh
+++ log-plots-create-chia.sh
@@ -1,3 +1,5 @@
 #log-plots-create-chia.sh
 #!/bin/bash
-cat $1 | grep "Starting phase" && cat $1 | grep "Time for phase"
+cat $1 | grep -i "Starting phase"
+cat $1 | grep -i "Time for phase"
+cat $1 | grep -i "Total time"
