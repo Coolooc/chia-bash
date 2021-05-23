e2eecc4eb91b0b8c88cca77c77da6606c2ebb6c1
Coolooc <egor-nal@yandex.ru>
2021-05-23 15:42:45 +0400

    Update log-farmer-chia.sh

--- log-farmer-chia.sh
+++ log-farmer-chia.sh
@@ -1,4 +1,5 @@
 #log-farmer-chia.sh
 #!/bin/bash
 echo "wait..."
-tail -f ~/.chia/mainnet/log/debug.log | grep farmer
+#tail -f ~/.chia/mainnet/log/debug.log | grep farmer
+tail -f ~/.chia/mainnet/log/debug.log | grep farm
