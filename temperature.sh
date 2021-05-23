87b45c5327738599a36339b238149f5753740f73
Coolooc <egor-nal@yandex.ru>
2021-05-23 15:01:28 +0400

    Update temperature.sh

--- temperature.sh
+++ temperature.sh
@@ -1,5 +1,16 @@
 #temperature.sh
 #!/bin/bash
+echo
+uptime
+echo
 sudo sensors
 sudo hddtemp /dev/sd?
+#echo
+#echo sda
 #sudo smartctl -a /dev/sda | grep -i temp
+#echo sdb
+#sudo smartctl -a /dev/sdb | grep -i temp
+#echo sdc
+#sudo smartctl -a /dev/sdc | grep -i temp
+#echo sdd
+#sudo smartctl -a /dev/sdd | grep -i temp
