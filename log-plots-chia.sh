daf52772f18cd104a50e6e1c7d4a2ca1c6d7ebbd
Coolooc <egor-nal@yandex.ru>
2021-05-24 22:34:57 +0400

    Update log-plots-chia.sh

--- log-plots-chia.sh
+++ log-plots-chia.sh
@@ -1,5 +1,13 @@
 #log-plots-chia.sh
 #!/bin/bash
-echo "wait..."
-#tail -f ~/.chia/mainnet/log/debug.log | grep plots
-tail -f ~/.chia/mainnet/log/debug.log | grep eligible
+cd ~
+rm -rf log-plots
+mkdir log-plots
+cd log-plots/
+cp ~/.chia/mainnet/log/debug.log ./debug191.log
+scp user1@10.1.2.189:/home/user1/.chia/mainnet/log/debug.log ./debug189.log
+scp user1@10.1.2.192:/home/user1/.chia/mainnet/log/debug.log ./debug192.log
+cat debug*.log | grep "[^0] plots were" > used_plots.txt
+less used_plots.txt
+cat debug*.log | grep "Found [^0] proof" > found_proof.txt
+less found_proof.txt
