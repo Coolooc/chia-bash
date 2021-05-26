53247f4dd6ee9bd0b80a394b7ffb43380cd603a4
Coolooc <egor-nal@yandex.ru>
2021-05-26 08:35:46 +0400

    add git plotman

--- install-chia.sh
+++ install-chia.sh
@@ -5,6 +5,7 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y git smartmontools lm-sensors hddtemp iotop
 #sudo apt install -y glances
+sudo sensors-detect
 #rm -rf ~/chia-blockchain/
 git clone https://github.com/Chia-Network/chia-blockchain.git -b latest --recurse-submodules
 cd chia-blockchain
@@ -12,5 +13,9 @@ sh install.sh
 . ./activate
 chia init
 chia version
+#https://github.com/ericaltendorf/plotman
+#pip install --force-reinstall git+https://github.com/ericaltendorf/plotman@main
+#plotman config generate
+#vim ~/.config/plotman/plotman.yaml
 deactivate
 echo Installed.
