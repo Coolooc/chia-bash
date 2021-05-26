14f835ad4b51cf64500beff05ca7c386fda3f01a
Coolooc <egor-nal@yandex.ru>
2021-05-26 13:39:00 +0400

    Update log-plots-used-chia.sh

--- log-plots-used-chia.sh
+++ log-plots-used-chia.sh
@@ -2,14 +2,14 @@
 #!/bin/bash
 cd ~
 
-cat ~/.chia/mainnet/log/debug.log | grep "[^0] plots were" > used_plots_0.txt
+cat ~/.chia/mainnet/log/debug.log* | grep "[^0] plots were" > used_plots_0.txt
 less used_plots_0.txt
 rm used_plots_0.txt
 
-cat ~/.chia/mainnet/log/debug.log | grep "[^0-1] plots were" > used_plots_0-1.txt
+cat ~/.chia/mainnet/log/debug.log* | grep "[^0-1] plots were" > used_plots_0-1.txt
 less used_plots_0-1.txt
 rm used_plots_0-1.txt
 
-cat ~/.chia/mainnet/log/debug.log | grep "Found [^0] proof" > found_proof.txt
+cat ~/.chia/mainnet/log/debug.log* | grep "Found [^0] proof" > found_proof.txt
 less found_proof.txt
 rm found_proof.txt
